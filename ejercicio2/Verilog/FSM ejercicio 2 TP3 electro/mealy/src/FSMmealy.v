91d7b06f19b938684b6045b05bd23f5bd4c945bc
Matias Fogg <mfogg@itba.edu.ar>
2018-11-14 23:57:13 -0300

    Add files via upload

--- ejercicio2/Verilog/FSM ejercicio 2 TP3 electro/mealy/src/FSMmealy.v
+++ ejercicio2/Verilog/FSM ejercicio 2 TP3 electro/mealy/src/FSMmealy.v
@@ -1,50 +1,56 @@
-module FSMmealy(clk,resetn,w,z);
-	input clk,resetn,w:
-	output z;
+module FSMmealy(clk,resetn,w,out);
+	input clk,resetn,w;
+	output out;
 	reg [2:1] y, Y, z;
-	parameter [2:1] A=2’b00, B=2'b01, C=2’b11, D=2’b10;	//los estados se definen con menos bits porque hay uno menos
+	parameter [2:1] A=2'b00, B=2'b01, C=2'b11, D=2'b10;	//los estados se definen con menos bits porque hay uno menos
 	
 	//comportamiento estado a estado
 	always @(w or y)
 		case(y)
 			A: if(w)
 			begin
-				z=0;
+				out=0;
 				Y<=B;
 			end else 
 			begin
-				z=0;
+				out=0;
 				Y<=A;
 			end
 			
 			B: if(w)
 			begin
-				z=0;
+				out=0;
 				Y<=C;
 			end else 
 			begin
-				z=0;
+				out=0;
 				Y<=A;
 			end
 			
 			C: if(w)
 			begin
-				z=0;
+				out=0;
 				Y<=C;
 			end else 
 			begin
-				z=0;
+				out=0;
 				Y<=D;
 			end
 			
 			D: if(w)
 			begin
-				z=1;
+				out=1;
 				Y<=B;
 			end else 
 			begin
-				z=0;
+				out=0;
 				Y<=A;
+			end
+
+			default:
+			begin
+			out=0;
+				Y<=2'bxx;
 			end
 		endcase
 	
